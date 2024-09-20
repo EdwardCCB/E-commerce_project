ea0f863605d640db34550c91de04615f8c4c647c
Eduardo Marino <eduardomarino1028@hotmail.com>
2024-09-19 23:03:41 -0500

    Creation of the e-commerce footer

---
+++ frontend/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Body.css'
+
+const Body = () => {
+    return (
+        <body className="body-div">
+
+        </body>
+    )
+}
+
+export default Body;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import '../styles/Footer.css'
+import logo from '../assets/logo.png';
+import facebook from '../assets/facebook.png';
+import instagram from '../assets/instagram.png';
+import twitter from '../assets/twitter-alt.png';
+import linkedin from '../assets/linkedin.png';
+
+const CommerceName = "WordMark";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="sb_footer section_padding">
+                <hr></hr>
+
+                <div className="sb_footer-links">
+                    <div className="sb_footer-logo">
+                        <img alt="" src={logo} className="footer-logo"/>
+                    </div>
+                    <div className="sb_footer-links-div">
+                        <h4>Discovery</h4>
+                        <a href="/new-season">
+                            <p>New Season</p>
+                        </a>
+                        <a href="/most-searched">
+                            <p>Most Searched</p>
+                        </a>
+                        <a href="/new-selled">
+                            <p>Most Selled</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links-div">
+                        <h4>About</h4>
+                        <a href="/help">
+                            <p>Help</p>
+                        </a>
+                        <a href="/shipping">
+                            <p>Shipping</p>
+                        </a>
+                        <a href="/afilliate">
+                            <p>Afilliate</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links-div">
+                        <h4>Info</h4>
+                        <a href="/contact-us">
+                            <p>Contact Us</p>
+                        </a>
+                        <a href="/privacy">
+                            <p>Privacy Policies</p>
+                        </a>
+                        <a href="/terms-and-conditions">
+                            <p>Terms & Conditions</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-social-div">
+                        <h5>Follow us</h5>
+                        <div className="socialmedia">
+                            <p><img src={facebook} alt=""/></p>
+                            <p><img src={instagram} alt=""/></p>
+                            <p><img src={twitter} alt=""/></p>
+                            <p><img src={linkedin} alt=""/></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="sb_footer-below">
+                <div className="sb_footer-copyright">
+                    <p className="copyright">&copy;{new Date().getFullYear()} {CommerceName} All Rights Reserverd.</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
