914caa27fb94aa3d778f94838005b14156459b4f
Eduardo Marino <eduardomarino1028@hotmail.com>
2024-09-23 00:34:59 -0500

    Creation of the product and popular product component. Implementation of the component to the App. Needs a filling correction in the product image.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,93 @@
 import React from 'react';
-import Body from "./components/Body.jsx";
+import { Sections } from "./components/Sections.jsx";
+import graphicProduct from "./assets/graphic-product.svg"
 import Footer from "./components/Footer.jsx";
 
 function App() {
+    const products = [
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+    ];
+    const popularProducts = [
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+        {
+            href: "#",
+            img: graphicProduct,
+            name: "Product name",
+            price: "999.99",
+        },
+    ];
     return (
         <div>
-            {/* Aquí va el resto del contenido de tu aplicación */}
-            <Body />
+            <>
+                <Sections
+                    title="Products"
+                    description="Order it for you or for your beloved ones"
+                    products={products}
+                />
+                <Sections
+                    title="Products"
+                    description="Our top selling product that you may like"
+                    products={popularProducts}
+                />
+            </>
             <Footer />
         </div>
     );
