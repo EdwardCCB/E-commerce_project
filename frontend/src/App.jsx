01738ec3369c2fa77577c47c60f86d0cfef93a03
francisco <franciscot.ussa2003@gmail.com>
2024-09-24 00:57:37 -0500

    its ready to merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-///import {Nav} from './components/Nav'
-//import { Present } from './components/Present';
+import {Nav} from './components/Nav'
+import { Present } from './components/Present';
 import { Brands } from './components/Brands';
 
 function App () {
   return(
     <div>
+      <Nav></Nav>
+      <Present></Present>
       <Brands></Brands>
     </div>
   )
